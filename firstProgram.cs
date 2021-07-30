5da0b4f8c32de2e17d72c07f0719672b7ff7f2d4
Florian Garcia <32206787+FloG16@users.noreply.github.com>
2021-07-30 14:25:22 +0200

    Create firstProgram.cs

---
+++ firstProgram.cs
@@ -0,0 +1,11 @@
+Console.WriteLine("Hello World!");
+
+/*Félicitations ! Vous avez exécuté votre premier programme C#. Il s’agit d’un programme simple qui affiche le message « Hello World! ». 
+Il a utilisé la méthode Console.WriteLine pour afficher ce message. Console est un type qui représente la fenêtre de console. WriteLine est une 
+méthode du type Console qui affiche une ligne de texte sur cette console de texte.
+
+Poursuivons notre exploration. Le reste de cette leçon explore l’utilisation du type string, qui représente du texte en C#. À l’instar du type Console, 
+le type string propose différentes méthodes. La méthode string fonctionne avec du texte.*/
+
+string aFriend = "Bill";
+Console.WriteLine(aFriend);
