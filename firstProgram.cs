8275a59c609dc88a3b98c8ea60a67cd62d887457
Florian Garcia <32206787+FloG16@users.noreply.github.com>
2021-07-30 14:27:21 +0200

    Update firstProgram.cs

--- firstProgram.cs
+++ firstProgram.cs
@@ -1,3 +1,6 @@
+/*source : https://docs.microsoft.com/fr-fr/dotnet/csharp/tour-of-csharp/tutorials/hello-world?tutorial-step=1*/
+
+
 Console.WriteLine("Hello World!");
 
 /*Félicitations ! Vous avez exécuté votre premier programme C#. Il s’agit d’un programme simple qui affiche le message « Hello World! ». 
@@ -9,3 +12,18 @@ le type string propose différentes méthodes. La méthode string fonctionne ave
 
 string aFriend = "Bill";
 Console.WriteLine(aFriend);
+
+/*La première ligne déclare une variable, aFriend , et lui assigne une valeur, « Bill ». La deuxième ligne affiche le nom.
+
+Vous pouvez assigner des valeurs différentes à toute variable que vous déclarez. Vous pouvez remplacer le nom par celui d’un de vos amis. 
+Ajoutez ces deux lignes dans la fenêtre interactive à la suite du code que vous avez déjà ajouté. Veillez à conserver la déclaration de la aFriend variable et 
+son affectation initiale.*/
+
+aFriend = "Maira";
+Console.WriteLine(aFriend);
+
+/*Notez que la même ligne de code affiche deux messages différents, selon la valeur stockée dans la variable aFriend.
+
+Vous avez peut-être également remarqué que le mot « Hello » manque dans les deux derniers messages. Nous allons résoudre ce problème maintenant. 
+Modifiez les lignes qui affichent le message comme suit :*/
+
